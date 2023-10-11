8fbadbff9047f79e6670b7811e2dfe6c208e5567
Ex4byt3 <jackdanielkerns@gmail.com>
2023-10-11 02:26:08 -0400

    ggpo module

---
+++ modules/godotggpo/config.py
@@ -0,0 +1,21 @@
+def can_build(env, platform):
+    return platform == "windows" # For now, GGPO isn't available on linux or mac
+
+def configure(env):
+    env.Append(CPPPATH=["#modules/godotggpo/sdk/include/"])
+
+    if env["platform"] == "windows":
+        if env["CC"] == "cl":
+            env.Append(LINKFLAGS=["GGPO.lib"])
+            env.Append(LIBPATH=["#modules/godotggpo/sdk/bin/win64"])
+        else:
+            env.Append(LIBS=["ggpo"])
+            env.Append(LIBPATH=["#modules/godotggpo/sdk/bin/win64"])
+
+def get_doc_classes():
+    return [
+        "GGPO",
+    ]
+
+def get_doc_path():
+    return "doc_class"
